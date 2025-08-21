42273e6e8c44c5468dcc25a61d49ba1b15784241
zzzyans <limzhiyan05@gmail.com>
2025-08-21 17:31:47 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Bong.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bong {
+    public static void main(String[] args) {
+        String line = "------------------------------";
+
+        System.out.println("Hello! I'm Bong!\nWhat can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}

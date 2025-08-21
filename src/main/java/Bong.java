162c8492a89879d3dac259ea939ce0ad3440930c
zzzyans <limzhiyan05@gmail.com>
2025-08-21 19:11:47 +0800

    Level 3. Mark as Done

--- src/main/java/Bong.java
+++ src/main/java/Bong.java
@@ -1,6 +1,34 @@
 import java.util.Scanner;
 
 public class Bong {
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+    
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+    
+        public String getStatusIcon() {
+            return (this.isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void mark() {
+            this.isDone = true;
+        }
+
+        public void unmark() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        } 
+    }
+
     public static void main(String[] args) {
         String line = "------------------------------";
 
@@ -10,7 +38,7 @@ public class Bong {
         Scanner scanner = new Scanner(System.in);
         String userInput;
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
 
         while (true) {
@@ -20,13 +48,28 @@ public class Bong {
                 break;
             } else if (userInput.equals("list")) {
                 int listNumber = 1;
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println("    " + listNumber + ". " + list[i]);
+                    System.out.println("    " + listNumber + ". " + list[i].toString());
                     listNumber++;
                 }
                 System.out.println(line);
+            } else if (userInput.startsWith("mark ")) {
+                String taskNumberString = userInput.substring(5);
+                int taskNumber = Integer.parseInt(taskNumberString);
+                list[taskNumber - 1].mark();
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("        " + list[taskNumber - 1].toString());
+                System.out.println(line);
+            } else if (userInput.startsWith("unmark ")) {
+                String taskNumberString = userInput.substring(7);
+                int taskNumber = Integer.parseInt(taskNumberString);
+                list[taskNumber - 1].unmark();
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("        " + list[taskNumber - 1].toString());
+                System.out.println(line);
             } else {
-                list[count] = userInput;
+                list[count] = new Task(userInput);
                 count++;
                 System.out.println("    added: " + userInput);
                 System.out.println(line);

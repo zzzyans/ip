c469929181ee8722ce899a6d29c7e68431e68209
zzzyans <limzhiyan05@gmail.com>
2025-08-21 17:47:59 +0800

    Level 1. Echo

--- src/main/java/Bong.java
+++ src/main/java/Bong.java
@@ -4,9 +4,25 @@ public class Bong {
     public static void main(String[] args) {
         String line = "------------------------------";
 
-        System.out.println("Hello! I'm Bong!\nWhat can I do for you?");
+        System.out.println("    Hello! I'm Bong!\n    What can I do for you?");
         System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println("    " + userInput);
+                System.out.println(line);
+            }
+        }
+
+        System.out.println("    Bye. Hope to see you again soon!");
+
+        scanner.close();
     }
 }

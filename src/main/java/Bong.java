58810b37106b8a1d69cd08d71256215be9d4e2f4
zzzyans <limzhiyan05@gmail.com>
2025-08-21 18:19:18 +0800

    Level 2. Add, List

--- src/main/java/Bong.java
+++ src/main/java/Bong.java
@@ -10,13 +10,25 @@ public class Bong {
         Scanner scanner = new Scanner(System.in);
         String userInput;
 
+        String[] list = new String[100];
+        int count = 0;
+
         while (true) {
             userInput = scanner.nextLine();
 
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                int listNumber = 1;
+                for (int i = 0; i < count; i++) {
+                    System.out.println("    " + listNumber + ". " + list[i]);
+                    listNumber++;
+                }
+                System.out.println(line);
             } else {
-                System.out.println("    " + userInput);
+                list[count] = userInput;
+                count++;
+                System.out.println("    added: " + userInput);
                 System.out.println(line);
             }
         }
